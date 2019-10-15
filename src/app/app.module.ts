2b64f1106af9355365f807800606721f2b6f051d
Ahmed Shaaban <ahmed.vuw@gmail.com>
2019-10-15 14:57:04 +1300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, ShoppingCartComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
