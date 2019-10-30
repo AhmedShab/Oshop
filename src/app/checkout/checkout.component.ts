75ede1fff3a1c4dd76a43c3cbccf533d9b2dae3e
Ahmed Shaaban <ahmed.vuw@gmail.com>
2019-10-31 12:08:54 +1300

    store userID

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { ShoppingCartService } from '../shopping-cart.service';
 import { Observable, Subscription } from 'rxjs';
 import { ShoppingCart } from '../models/shopping-cart';
 import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,25 +14,33 @@ import { OrderService } from '../order.service';
 export class CheckoutComponent implements OnInit, OnDestroy {
   shipping = {} as ShippingAddress;
   cart: ShoppingCart;
+  userId: string;
   cart$: Observable<ShoppingCart>;
-  subscription: Subscription;
+  orderSubscription: Subscription;
+  userSubscription: Subscription;
 
   constructor(
     private cartService: ShoppingCartService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private authService: AuthService
   ) {}
 
   async ngOnInit() {
     this.cart$ = await this.cartService.getCart();
-    this.subscription = this.cart$.subscribe(cart => (this.cart = cart));
+    this.orderSubscription = this.cart$.subscribe(cart => (this.cart = cart));
+    this.userSubscription = this.authService.user$.subscribe(
+      user => (this.userId = user.uid)
+    );
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.orderSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
   placeOrder() {
     let order = {
+      userId: this.userId,
       datePlaced: new Date().getTime(),
       shipping: this.shipping,
       items: this.cart.items.map(item => {
